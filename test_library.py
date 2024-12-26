e2e64ab40fc888e21448e707ca34c3c7ebaf7df2
Dhruv-quest <dhruv.p@questlabs.biz>
2024-12-26 10:18:06 +0530

    Add test for Retur the Book

--- test_library.py
+++ test_library.py
@@ -17,8 +17,18 @@ class LibraryTestCase(unittest.TestCase):
         self.library.add_book(book)
         self.library.borrow_book(book.isbn)
         self.assertTrue(book.is_borrowed)
+
     def test_borrow_nonexistent_book(self):
         with self.assertRaises(ValueError):
             self.library.borrow_book("xyz")
+
+    def test_return_book(self):
+        book = Book(isbn="1234567890", title="snake", author="abc", year=1958)
+        self.library.add_book(book)
+        self.library.borrow_book(book.isbn)
+        self.library.return_book(book.isbn)
+        self.assertFalse(book.is_borrowed)
+
+
 if __name__ == '__main__':
     unittest.main()
